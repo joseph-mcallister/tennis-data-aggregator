0246b3c1ba728bb9ef1fcc80645c1ea6f236c00d
Nick Clifford <nick@nickclifford.me>
2020-06-03 14:39:32 -0500

    clean up UTR source, fix validation error

--- src/sources/utr.ts
+++ src/sources/utr.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import config from '../config';
 import { Player } from '../models/Player';
 import { UTREntry } from '../models/UTREntry';
-import { totalmem } from 'os';
 import assert from 'assert';
+import { UniqueConstraintError } from 'sequelize';
 
 export class UTR {
 	private jwt: string | null = null;
@@ -29,11 +29,12 @@ export class UTR {
 		const maxUtr = 13.0;
 		const step = 0.02;
 		for (let currMin = minUtr; currMin + step < maxUtr; currMin += step) {
-
-			console.log({currMin, currMax: currMin + step});
+			console.log({ currMin, currMax: currMin + step });
 			const res = await axios.get<UTRResponse>(
 				'https://agw-prod.myutr.com/v2/search/players' +
-					`?top=100&skip=0&gender=M&primaryTags=Junior&utrMin=${currMin}&utrMax=${currMin + step}&utrType=verified&utrTeamType=singles`,
+					`?top=100&skip=0&gender=M&primaryTags=Junior&utrMin=${currMin}&utrMax=${
+						currMin + step
+					}&utrType=verified&utrTeamType=singles`,
 				{ headers: this.headers }
 			);
 
@@ -48,22 +49,27 @@ export class UTR {
 					utrProfileId: profileIds
 				}
 			});
-			const existingPlayerIds = existingPlayers.map(p => p.id);
 
-			// create new players if there aren't any associated with the given UTR profile
 			const newPlayers = await Player.bulkCreate(
 				responseProfiles
-					.filter(p => !existingPlayerIds.includes(p.id))
-					.map(p => ({ 
-						name: p.displayName, 
-						utrProfileId: p.id, 
-						gradClassName: p.playerHighSchoolDetails ? p.playerHighSchoolDetails.gradClassName : null, 
-						gradYear: p.playerHighSchoolDetails ? p.playerHighSchoolDetails.gradYear : null,
-						latitude: p.location && p.location.latLng ? p.location.latLng[0]: null,
-						longitude: p.location && p.location.latLng ? p.location.latLng[1]: null,
-						location: p.location && p.location.display ? p.location.display : null
-					}))
-			);
+					.map(p => ({
+						name: p.displayName,
+						utrProfileId: p.id,
+						gradClassName: p.playerHighSchoolDetails?.gradClassName,
+						gradYear: p.playerHighSchoolDetails?.gradYear,
+						latitude: p.location?.latLng?.[0],
+						longitude: p.location?.latLng?.[1],
+						location: p.location?.display
+					})),
+				// adds `ON CONFLICT DO NOTHING` to the query
+				// because UTR ids are unique, only creates new players if they don't exist
+				{ ignoreDuplicates: true }
+			).catch(err => {
+				if (err instanceof UniqueConstraintError) {
+					console.log(err.errors);
+				}
+				throw err;
+			});
 
 			const allPlayers = existingPlayers.concat(newPlayers);
 
@@ -90,13 +96,13 @@ export interface UTRResponse {
 			myUtrSingles: number;
 			myUtrDoubles: number;
 			location: {
-				display: string;
-				latLng: number[];
-			};
+				display: string; // never null, just empty string
+				latLng: number[] | null;
+			} | null;
 			playerHighSchoolDetails: {
 				gradClassName: string;
 				gradYear: Date;
-			};
+			} | null;
 		};
 	}>;
 }
